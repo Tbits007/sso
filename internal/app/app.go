5f97e73d8cb37f756d562748c8c0b2ca9e723512
Tbits007 <brusolis228@gmail.com>
2025-04-06 22:43:15 +0300

    +graceful shutdown

---
+++ internal/app/app.go
@@ -0,0 +1,31 @@
+package app
+
+import (
+	"log/slog"
+	"sso/internal/app/grpcapp"
+	"time"
+)
+
+type App struct {
+	GRPCServer *grpcapp.App
+}
+
+func New(
+	log *slog.Logger,
+	grpcPort int,
+	storagePath string,
+	tokenTTL time.Duration,
+) *App {
+	storage, err := postgres.New(storagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	authService := auth.New(log, storage, storage, storage, tokenTTL)
+
+	grpcApp := grpcapp.New(log, authService, grpcPort)
+
+	return &App{
+		GRPCServer: grpcApp,
+	}
+}
